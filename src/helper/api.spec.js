c5550d7615ce80f02731aada2fd0fa95c166ff66
SPatel <sonal.patel@justgiving.com>
2018-05-24 13:33:00 +0100

    my unit test to check the fetch

---
+++ src/helper/api.spec.js
@@ -0,0 +1,39 @@
+import { getCharityById, getCharityDonationsById, AppId } from "./Api";
+
+window.fetch = jest.fn(async () => ({}));
+
+//testing the API function for the charity content
+describe("getCharityById", () => {
+  it("should check the fetch was called", async () => {
+    const charityId = 2357;
+    const options = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    };
+    await getCharityById(charityId);
+    expect(fetch).toBeCalledWith(
+      `https://api.justgiving.com/${AppId}/v1/charity/${charityId}`,
+      options
+    );
+  });
+});
+
+//teting the API function for the charity donations content
+describe("getCharityDonationsById", () => {
+  it("should check the fetch was called", async () => {
+    const charityId = 2357;
+    const options = {
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      }
+    };
+    await getCharityDonationsById(charityId);
+    expect(fetch).toBeCalledWith(
+      `https://api.justgiving.com/${AppId}/v1/charity/${charityId}/donations`,
+      options
+    );
+  });
+});
