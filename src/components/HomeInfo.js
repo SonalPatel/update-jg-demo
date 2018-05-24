083a09d20235b2ad6a4d995b54c9cbd2d10f1e27
SPatel <sonal.patel@justgiving.com>
2018-05-24 11:06:26 +0100

    moved content on landing page to a new component to tidy it up

--- src/components/HomeInfo.js
+++ src/components/HomeInfo.js
@@ -1,15 +1,38 @@
 import React from "react";
+/* import BrowserRouter from 'react-router-dom' */
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 class HomeInfo extends React.Component {
   render() {
     return (
-      <div className="flex-grid mt">
-        <div className="col">
-          <p>Hello </p>
-          <p>Hello </p>
-          <p>Hello </p>
-          <p>Hello </p>
-          <p>Hello </p>
+      <div>
+        <div className="content-wrap">
+          <h2>Welcome to my assesments</h2>
+          <p>
+            Following the feedback from the first assesment I have created a new
+            app to show the updated code
+          </p>
+        </div>
+
+        <div className="flex-grid mt">
+          <div className="col content-description">
+            <h3>
+              The <Link to="/updated">Updated assesment</Link> has the following
+              changes:
+            </h3>
+            <ul>
+              <li> TO DO </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+              <li> x </li>
+            </ul>
+          </div>
         </div>
       </div>
     );
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,37 +1,13 @@
 import React from "react";
 import "../styles/App.css";
-//import HomeInfo from "../components/HomeInfo.js";
+import HomeInfo from "../components/HomeInfo.js";
 //import Footer from "../components/Footer.js";
 
 class HomeContainer extends React.Component {
   render() {
     return (
       <div className="container mt">
-        <div className="content-wrap">
-          <h2>Welcome to my assesments</h2>
-          <p>
-            Following the feedback from the first assesment I have created a new
-            app to show the updated code
-          </p>
-        </div>
-
-        <div className="flex-grid mt">
-          <div className="col">
-            <h2>The updated assesment has the following changes:</h2>
-            <ul>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-              <li> x </li>
-            </ul>
-          </div>
-        </div>
+        <HomeInfo />
       </div>
     );
   }
