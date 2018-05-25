116c78b1877ce724e0b1c162481b288c048ca4e1
SPatel <sonal.patel@justgiving.com>
2018-05-25 12:23:09 +0100

    amends to my  render test, it works when i manually change the charitydonation id to an invalid one. (so atm it's a manual test)

--- src/components/tests/DonationsUdate-test.js
+++ src/components/tests/DonationsUdate-test.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { shallow, mount, render } from "enzyme";
+import CharityInfoUpdate from "../components/CharityInfoUpdate";
 import DonationsUpdate from "../components/DonationsUpdate";
 
-//describe what is being tested: testing if my page still renders if there are no donations
+//testing if my page still renders if there are no donations
 describe("should still render the page if there are no donations", () => {
   // make our assertion and what we expect to happen
-  it("should render the page without an error", () => {
+  it("should render the page with the charity conent", () => {
     expect(
-      shallow(<DonationsUpdate />).exists(
-        <div className="donations-wrapper" id="latest-donations">
-          {" "}
-        </div>
+      shallow(<CharityInfoUpdate />).exists(
+        <div className="brand-banner"> </div>
       )
     ).toBe(true);
   });
